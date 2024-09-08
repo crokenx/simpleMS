8fd44a3b8769877fad7681a7723f1852cc1524a5
crokenx <quesadadiaz18@gmail.com>
2024-09-08 15:22:32 -0500

    feat(ms): initial commit, just saving changes

---
+++ src/main/java/com/orders/crx/simpleMS/SimpleMsApplication.java
@@ -0,0 +1,13 @@
+package com.orders.crx.simpleMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orders/crx/simpleMS/SimpleMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orders.crx.simpleMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
